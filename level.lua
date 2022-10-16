97d6033d13c7fdda9878bbd0a75f7437bdb46899
glebi574 <glebi574@gmail.com>
2022-10-16 20:55:28 +0300

    Update

--- level.lua
+++ level.lua
@@ -1,4 +1,4 @@
 
-pewpew.configure_player(0, {camera_distance = 600fx})
+pewpew.configure_player(0, {camera_distance = 300fx})
 local id = pewpew.new_customizable_entity(0fx, 0fx)
 pewpew.customizable_entity_set_mesh(id, '/dynamic/mesh.lua', 0)
